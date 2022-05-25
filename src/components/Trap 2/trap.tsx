e85ec2a59d44b8fc05a6a66c3cbf7ff57b4f7bc3
MatheusVAZ <matheusavaz12@gmail.com>
2022-05-25 16:41:19 -0300

    add a new trap

---
+++ src/components/Trap 2/trap.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './trap.css'
+import { TILE_SIZE } from '../../settings/constants'
+
+  export function Trap() {
+  return (
+    <div 
+      style={{
+          position:"absolute",
+          bottom: TILE_SIZE * 8,
+          left: TILE_SIZE * 11,
+          width: TILE_SIZE,
+          height: TILE_SIZE,
+          backgroundImage: "url(./assets/TRAP.png)",
+          backgroundRepeat: 'no-repeat',
+          animation: "trap-animation 1s steps(8) infinite"
+      }}
+    />
+  )
+}
